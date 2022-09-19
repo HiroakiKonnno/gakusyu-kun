16e8bd06bcb75e81840ab9b56d8c19337e7497eb
Hiroaki Konno <all_for_joy_h0708@yahoo.co.jp>
2022-09-19 20:59:41 +0900

    aws修正

--- config/puma.rb
+++ config/puma.rb
@@ -56,7 +56,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
 
 app_root = File.expand_path("../..", __FILE__)
-bind "unix:///#{app_root}/tmp/sockets/puma.sock"
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
 unless ENV.fetch("RAILS_ENV", "development") == "development"
   stdout_redirect "#{app_root}/log/development.log", "#{app_root}/log/puma.stderr.log", true
